cece47625b1db51e43d0d5d2ef526942a8fc9ec4
ShriramG24 <shrirambhavin@gmail.com>
2022-11-12 02:38:39 -0500

    implement interests

--- generateClassPath.py
+++ generateClassPath.py
@@ -4,48 +4,6 @@ from parser import main, cics_courses, all_topics
 
 pprint.pprint(all_topics)
 courses = main(cics_courses)
-# {
-#   "100C": {
-#     "121": [], 
-#     "187": ["121"]
-#   },
-#   "200C": {
-#     "220": ["187"],
-#     "240": ["132", "187"],
-#     "230": ["187"],
-#     "250": ["132", "187"]
-#   },
-#   "300C": {
-#     "311": ["187", "250"],
-#     "305": ["220", "230", "240", "250"]
-#   },
-#   "300E": {
-#     "383": ["220", "240"],
-#     "345": ["187"],
-#     "389": ["220", "240"],
-#     "360": ["230"],
-#     "377": ["230"]
-#   },
-#   "400E": {
-#     "420": ["187", "220"],
-#     "446": ["220", "240"],
-#     "453": ["230", "377"],
-#     "489": ["383"]
-#   }
-# }
-
-{
-  'COMPSCI 250': {
-    'prereqs': ['COMPSCI 187', 'MATH 132'], 
-    'professors': ['Ghazaleh Parvini'], 
-    'description': "Basic concepts of discrete mathematics useful to computer science:  set theory, strings and formal \
-                    languages, propositional and predicate calculus, relations and functions, basic number theory. \
-                    Induction and recursion: interplay of inductive definition, inductive proof, and recursive algorithms. \
-                    Graphs, trees, and search. Finite-state machines, regular languages, nondeterministic finite automata, \
-                    Kleene's Theorem.", \
-    'credits': 4.0
-  }
-}
 
 # Get next best course whose prereqs are satisfied
 def getCourse(courses, reqs, lvl):
@@ -61,7 +19,7 @@ def getCourse(courses, reqs, lvl):
       return c
 
 
-def findPath(courses: dict, taken: list, topics: dict, interests: list):
+def findPath(courses: dict, taken: list, topics: dict, interests: set):
   # Pre-populate schedule with taken/core courses
   majorReqs = {
     "100C": set(["121", "187"]),
@@ -72,7 +30,10 @@ def findPath(courses: dict, taken: list, topics: dict, interests: list):
   }
 
   path = []
-
+  impCourses = []
+  for i in interests:
+    impCourses.extend(all_topics[i])
+  
   def dfs(course):
     if course in path:
       return
