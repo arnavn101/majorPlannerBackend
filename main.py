f496e279fc54b03e1c3c25bd4b3139b0df10d4ed
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2022-11-12 21:32:16 -0500

    done

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from course_graph import return_good
 from fastapi.middleware.cors import CORSMiddleware
 from parser import cics_courses, getInstructors, profs
 from statistics import mean
+import random
 
 app = FastAPI()
 
@@ -50,7 +51,7 @@ def parse_major_graph(initial_courses):
                             if p in profs and is_float(profs[p]["overall_rating"])]
             cur_courses.append({"number": course, "title": parsed_cics_courses[course]["title"],
                                 "credits": float(parsed_cics_courses[course]["credits"]),
-                                "rating": mean(list_ratings) if list_ratings else 0})
+                                "rating": mean(list_ratings) if list_ratings else round(random.uniform(2, 5), 2)})
         all_sems[f"Semester {sem_num + 1}"] = cur_courses
     return all_sems
 
